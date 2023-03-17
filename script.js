815a1fd561f6c414a95e4f96d2b8e9b7dc9acb57
Nidhisahu57 <110117913+Nidhisahu57@users.noreply.github.com>
2023-03-17 15:44:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("btn");
+
+let randomColorGenerator = function randomColor (){
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+    for(let i = 0; i<6; i++){
+        cons = cons + val[Math.floor(Math.random()*16)];
+    }
+    return cons;
+};
+
+function rdmclrchnger(){
+    document.body.style.backgroundColor = randomColorGenerator();
+}
+btn.addEventListener("click", rdmclrchnger);
